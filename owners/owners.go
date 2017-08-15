f6bf7501919b4b48b7dfbb56271ab5af0e3685b9
Mitchell Turner <mturner@threatconnect.com>
2017-08-15 08:52:36 -0400

    refactor

--- owners/owners.go
+++ owners/owners.go
@@ -1,15 +1,18 @@
 package owners
 
+// SingleOwner is the base Owner structure
+type SingleOwner struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
 // Result represents the JSON returned from /v2/owners
 type Result struct {
 	Status string `json:"status"`
 	Data   struct {
-		ResultCount int `json:"resultCount"`
-		Owners      []struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-			Type string `json:"type"`
-		} `json:"owner"`
+		ResultCount int           `json:"resultCount"`
+		Owners      []SingleOwner `json:"owner"`
 	} `json:"data"`
 }
 
@@ -17,11 +20,7 @@ type Result struct {
 type Owner struct {
 	Status string `json:"status"`
 	Data   struct {
-		ResultCount int `json:"resultCount"`
-		Owner       struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-			Type string `json:"type"`
-		} `json:"owner"`
+		ResultCount int         `json:"resultCount"`
+		Owner       SingleOwner `json:"owner"`
 	} `json:"data"`
 }
